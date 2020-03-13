7353612809f8a7e1d8f15cf5e74f9ab9cdef57a8
Destineei <59798091+Destineei@users.noreply.github.com>
2020-03-13 12:57:17 +0100

    Initial commit

---
+++ sources/js/script.js
@@ -0,0 +1,40 @@
+// const container = document.querySelector(".container")
+// let intLenghtContainer = container.clientHeight
+
+// window.onscroll = function() {move()};
+// console.log(document.documentElement.scrollTop);
+// console.log(intLenghtContainer);
+// var i = 0;
+// function move() {
+//     if (i == 0) {
+//         i = 1;
+//         var elem = document.querySelector('.myBar');
+//         var id = setInterval(frame, 10);
+//         function frame() {
+//             if (document.documentElement.scrollTop >= intLenghtContainer) {
+//                 clearInterval(id);
+//                 i = 0;
+//             }
+//             else {
+//                 console.log("ELSE");
+//                 elem.style.width = document.documentElement.scrollTop + "%";
+//                 // console.log(document.documentElement.scrollTop);
+//                 // console.log(intLenghtContainer);
+                
+//             }
+//         }
+//     }
+// }
+// const container = document.querySelector(".container")
+// let intLenghtContainer = container.clientHeight
+// const accueil = document.querySelector("#accueil")
+// let intLenghtAccueil = accueil.clientHeight
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.querySelector(".myBar").style.width = scrolled + "%";
+}
