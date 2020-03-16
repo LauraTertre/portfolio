bd7d11f992966f0d9d20d9131712b1afdfb45d9e
Destineei <59798091+Destineei@users.noreply.github.com>
2020-03-16 14:48:49 +0100

    firstcommit

--- sources/js/script.js
+++ sources/js/script.js
@@ -38,3 +38,42 @@ function myFunction() {
     let scrolled = (winScroll / height) * 100;
     document.querySelector(".myBar").style.width = scrolled + "%";
 }
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                            Projects
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Projects - More information
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+// const projectHover = document.querySelector(".project-hover")
+// const projectBlockMore = document.querySelector(".project-block-more")
+// const projectContentMore = document.querySelector('.project-content-more')
+
+// projectHover.addEventListener('click', () => {
+//     projectContentMore.classList.add('open')
+//     projectBlockMore.classList.add('open')
+// })
+
+const projectHover = document.querySelectorAll(".project-hover")
+const projectBlockMore = document.querySelectorAll(".project-block-more")
+const projectContentMore = document.querySelector('.project-content-more')
+
+projectHover.forEach((choice, index)=> {
+    choice.addEventListener('click', ()=> {
+        projectBlockMore.forEach((content)=> {
+            content.classList.remove('open')
+        })
+        projectContentMore.classList.add('open')
+        projectBlockMore[index].classList.add('open')
+    })
+})
+
+const buttonOff = document.querySelectorAll(".buttonOff")
+
+buttonOff.forEach((buttonOffChoice)=> {
+    buttonOffChoice.addEventListener('click', () => {
+        projectBlockMore.forEach((content)=> {
+            content.classList.remove('open')
+        })
+})
+})
