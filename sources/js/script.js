d4634ba97b0274c08fce2c53bca60b6c9618b84c
Destineei <59798091+Destineei@users.noreply.github.com>
2020-03-18 12:13:26 +0100

    Delete comment & page useless

--- sources/js/script.js
+++ sources/js/script.js
@@ -1,35 +1,3 @@
-// const container = document.querySelector(".container")
-// let intLenghtContainer = container.clientHeight
-
-// window.onscroll = function() {move()};
-// console.log(document.documentElement.scrollTop);
-// console.log(intLenghtContainer);
-// var i = 0;
-// function move() {
-//     if (i == 0) {
-//         i = 1;
-//         var elem = document.querySelector('.myBar');
-//         var id = setInterval(frame, 10);
-//         function frame() {
-//             if (document.documentElement.scrollTop >= intLenghtContainer) {
-//                 clearInterval(id);
-//                 i = 0;
-//             }
-//             else {
-//                 console.log("ELSE");
-//                 elem.style.width = document.documentElement.scrollTop + "%";
-//                 // console.log(document.documentElement.scrollTop);
-//                 // console.log(intLenghtContainer);
-                
-//             }
-//         }
-//     }
-// }
-// const container = document.querySelector(".container")
-// let intLenghtContainer = container.clientHeight
-// const accueil = document.querySelector("#accueil")
-// let intLenghtAccueil = accueil.clientHeight
-
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
@@ -45,15 +13,6 @@ function myFunction() {
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     Projects - More information
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-// const projectHover = document.querySelector(".project-hover")
-// const projectBlockMore = document.querySelector(".project-block-more")
-// const projectContentMore = document.querySelector('.project-content-more')
-
-// projectHover.addEventListener('click', () => {
-//     projectContentMore.classList.add('open')
-//     projectBlockMore.classList.add('open')
-// })
-
 const projectHover = document.querySelectorAll(".project-hover")
 const projectBlockMore = document.querySelectorAll(".project-block-more")
 const projectContentMore = document.querySelector('.project-content-more')
